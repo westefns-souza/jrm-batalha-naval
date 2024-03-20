d99a664864d815eefc2aec0ec17a82456487475e
westefns-souza <westefns@outlook.com.br>
2024-03-20 20:50:59 -0300

    associando duas sessões

--- server.js
+++ server.js
@@ -3,32 +3,43 @@ const WebSocket = require('ws');
 const port = 8080;
 const wss = new WebSocket.Server({ port: port });
 
-let conectados = 0;
-let partidas = [];
-let jogadores = [];
+let partida = {
+    player1: null,
+    player2: null,
+    shared: {
+        espera: true,
+        mensagem: "",
+        tabuleiro_player1: [],
+        tabuleiro_player2: []
+    }
+}
 
 wss.on('connection', function connection(ws) {
-    conectados++;
-    console.log('Um cliente se conectou, agora são: ', conectados);
+    console.log('Novo cliente conectado');
 
-    ws.on('message', function incoming(message) {
-        console.log('Mensagem recebida:', message.toString());
-    });
-
-    ws.on('close', function close() {
-        conectados--;
-        console.log('Um cliente se desconectou, agora são: ', conectados);
-    });
-
-    ws.send('Bem vindo cliente');
-    
-    if (conectados % 2 != 0) {
-        ws.send('Você é o jogador N° 1');
-        ws.send('Aguarde o jogador N° 2');
+    if (partida.player1 === null) {
+        partida.player1 = ws;
+        ws.send('Aguardando pelo segundo jogador...');
     } else {
-        ws.send('Você é o jogador N° 2');
-        ws.send('partida iniciada!');
+        partida.player2 = ws;
+        ws.send('Partida começará. Prepare-se!');
+
+        startGame(partida);
     }
+
+    ws.on('close', function () {
+        if (ws === partida.player1) {
+            partida.player1 = null;
+        } else if (ws === partida.player2) {
+            partida.player2 = null;
+        }
+    });
 });
+
+function startGame(partida) {
+    partida.player1.send('A partida começou');
+    partida.player2.send('A partida começou');
+}
+
 
 console.log(`servidor rodando na porta ${port}`);
